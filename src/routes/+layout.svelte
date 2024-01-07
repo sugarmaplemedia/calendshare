1e2f850fb4919173e2f6a616b5318df56496cf6e
Harrison Bouche <harrison@sm.media>
2024-01-07 13:34:51 -0500

    feat: add proper-ish logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,13 +26,27 @@
 
 <Modal components={modalRegistry} />
 
+<svelte:head>
+	<title>Calendshare</title>
+	<meta
+		name="description"
+		content="Your free-to-use app for finding a time in the week with others."
+	/>
+
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+	<link rel="manifest" href="/site.webmanifest" />
+</svelte:head>
+
 <!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<a href="/">
+				<a href="/" class="flex gap-2 items-center">
+					<img src="/icon-white.svg" alt="" class="h-6" />
 					<strong class="text-xl uppercase">Calendshare</strong>
 				</a>
 			</svelte:fragment>
